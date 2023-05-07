a4fa9b85558fc156a642885a1f1d2225c2624665
SpiderTheus <108964797+SpiderTheus@users.noreply.github.com>
2023-05-06 23:44:12 -0300

    Adcionado o js do butao sim/ finalizando o projeto

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-const butao = document.querySelector(".nop")
+const butaoN = document.querySelector(".nop")
 
-console.log(butao)
-
-butao.addEventListener("mouseover", function() {
-    butao.style.top =  Math.random() * 400 + "px"
-    butao.style.left =  Math.random() * 400 + "px"
-    butao.style.buttom =  Math.random() * 400 + "px"
-    butao.style.rigth =  Math.random() * 400 + "px"
+butaoN.addEventListener("mouseover", function() {
+    butaoN.style.top =  Math.random() * 400 + "px"
+    butaoN.style.left =  Math.random() * 400 + "px"
+    butaoN.style.buttom =  Math.random() * 400 + "px"
+    butaoN.style.rigth =  Math.random() * 400 + "px"
 })
 
 
+const butaoS = document.querySelector(".sim") 
+let n
+butaoS.addEventListener("click", function () {
+    if(n === 1){
+        mensagem();
+    }
+    const coracao = document.getElementById("img")
+    coracao.classList.add("core");
+    const akai = document.getElementById("akai")
+    akai.classList.add("core")
+    butaoN.remove()
+    const titulo = document.querySelector("h1")
+    titulo.textContent = "I Love You"
+
+    butaoS.textContent = "clique aqui"
+     n = 1
+})
+/* butaoS.addEventListener("click", function(){
+   
+})   
+ */
+function mensagem(){
+    window.location.href = "https://www.youtube.com/watch?v=rU_7krLXJiQ"
+}
